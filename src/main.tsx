c0951fe27261e252bd671c54c46e964c938f080e
Forkeh <forkeh@gmail.com>
2024-03-13 11:47:37 +0100

    kino selector uses backend

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,20 @@ import "./index.css";
 import AuthProvider from "./contexts/AuthProvider.tsx";
 import { BrowserRouter } from "react-router-dom";
 import KinoProvider from "./contexts/KinoProvider.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <BrowserRouter>
-            <AuthProvider>
-                <KinoProvider>
-                    <App />
-                </KinoProvider>
-            </AuthProvider>
+            <QueryClientProvider client={queryClient}>
+                <AuthProvider>
+                    <KinoProvider>
+                        <App />
+                    </KinoProvider>
+                </AuthProvider>
+            </QueryClientProvider>
         </BrowserRouter>
     </React.StrictMode>
 );
--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -1,46 +1,45 @@
 import StartPageButton from "@/components/core/StartPageButton";
+import { useToast } from "@/components/ui/use-toast";
 import { useKino } from "@/contexts/KinoProvider";
+import { IKino } from "@/models/kino";
+import { getKinos } from "@/services/apiFacade";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-
-const DUMMYCINEMA = [
-    {
-        name: "Imperial",
-        city: "København",
-    },
-    {
-        name: "Big Bio",
-        city: "Aalborg",
-    },
-    {
-        name: "Grande Bio",
-        city: "Esbjerg",
-    },
-];
-
-
 export default function StartPage() {
     const { setKino } = useKino();
-    const [kinos, setKinos] = useState(DUMMYCINEMA);
+    const [kinos, setKinos] = useState<IKino[] | null>(null);
     const navigate = useNavigate();
+    const { toast } = useToast();
+    console.log(kinos);
+
+    //const queryClient = useQueryClient();
+
+    // const { isSuccess, isLoading, isError, data, error } = useQuery("cinemas", getKinos);
 
     useEffect(() => {
-        //hent alle biografer
-    }, [])
-
-    
+        getKinos()
+            .then((data) => setKinos(data))
+            .catch(() => {
+                toast({
+                    title: "Something went wrong!",
+                    description: `Could not find the cinemas in our system. Please reload the webpage or try again at a later time.`,
+                    variant: "destructive",
+                });
+            });
+    }, []);
 
     const handleClick = (chosenCinema: string) => {
         setKino(chosenCinema);
         navigate("/movies", { replace: true });
-    }
-    
+    };
+
     return (
         <>
             <h1>Vælg Biograf</h1>
-            {kinos.map((kino) => (
-                <StartPageButton name={kino.name} city={kino.city} onClick={handleClick} />
+            {kinos?.map((kino) => (
+                <StartPageButton key={kino.id} name={kino.name} city={kino.city} onClick={handleClick} />
             ))}
         </>
     );
