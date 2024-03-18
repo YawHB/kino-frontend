8b986cca8641cd7d15726a761343c01fa7cf9836
Chris Skov Sørensen <chso0002@stud.kea.dk>
2024-03-18 14:52:07 +0100

    KINO-31 - linked ScreeningTime to ScreeningPage

--- src/components/layouts/Screenings/ScreeningTime.tsx
+++ src/components/layouts/Screenings/ScreeningTime.tsx
@@ -1,5 +1,6 @@
 import { IScreening } from "@/models/screening";
 import { toHourMinuteFormat } from "@/utils/dateUtils";
+import {Link, useNavigate} from "react-router-dom";
 
 interface Props {
   screening: IScreening;
@@ -9,14 +10,18 @@ export default function ScreeningTime({ screening }: Props) {
   function handleClick() {
     console.log(screening);
     //TODO: navigate to screening page, to reserve seats
+
+
   }
   return (
-    <article
-      className="w-full cursor-pointer rounded-sm bg-green-500 p-2 text-center text-white transition-all hover:bg-green-400"
-      onClick={handleClick}
-    >
-      <h3>{screening.auditorium.name}</h3>
-      <h3 className="font-bold">{toHourMinuteFormat(screening.startTime)}</h3>
-    </article>
+      <Link to="/screening">
+      <article
+        className="w-full cursor-pointer rounded-sm bg-green-500 p-2 text-center text-white transition-all hover:bg-green-400"
+        onClick={handleClick}
+      >
+        <h3>{screening.auditorium.name}</h3>
+        <h3 className="font-bold">{toHourMinuteFormat(screening.startTime)}</h3>
+      </article>
+      </Link>
   );
 }
