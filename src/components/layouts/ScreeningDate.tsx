85059044dccd046ed243b3dc66a23f613bcab9b8
AliHMohammad <alihmohammad@hotmail.com>
2024-03-18 11:27:32 +0100

    now displays screenings for movie

--- src/components/layouts/ScreeningDate.tsx
+++ src/components/layouts/ScreeningDate.tsx
@@ -1,30 +1,29 @@
+import { IScreening } from "@/models/screening";
+
 type Props = {
     date: string;
+    screenings: IScreening[]
 };
 
 
 
-export default function ScreeningDate({ date }: Props) {
-
-
+export default function ScreeningDate({ date, screenings }: Props) {
 
     return (
         <>
             <div className="flex flex-col gap-2">
                 <h2>{date}</h2>
-                <div className="bg-green-500">
-                    <h3>21:30</h3>
-                </div>
-                <div className="bg-green-500">
-                    <h3>21:30</h3>
-                </div>
-                <div className="bg-green-500">
-                    <h3>21:30</h3>
-                </div>
-                <div className="bg-green-500">
-                    <h3>21:30</h3>
-                </div>
+                {screenings?.map((s) => (
+                    <div key={s.id} className="bg-green-500">
+                        <h3>{s.startTime}</h3>
+                    </div>
+                ))}
             </div>
         </>
     );
 }
+/*
+<div className="bg-green-500">
+                    <h3>21:30</h3>
+                </div>
+*/
--- src/components/layouts/ScreeningOverview.tsx
+++ src/components/layouts/ScreeningOverview.tsx
@@ -5,7 +5,6 @@ import { IScreening } from "@/models/screening";
 import { getMovieScreeningsInCinema } from "@/services/apiFacade";
 import { useKino } from "@/contexts/KinoProvider";
 
-
 const TODAY = new Date();
 
 const OPTIONS: Intl.DateTimeFormatOptions = {
@@ -16,10 +15,9 @@ const OPTIONS: Intl.DateTimeFormatOptions = {
 
 type Props = {
     movieId: number;
-}
+};
 
-
-export default function ScreeningOverview({movieId}: Props) {
+export default function ScreeningOverview({ movieId }: Props) {
     const [screenings, setScreenings] = useState<IScreening[] | null>(null);
     const { id } = useKino();
     const week: string[] = [];
@@ -31,25 +29,26 @@ export default function ScreeningOverview({movieId}: Props) {
 
     useEffect(() => {
         const endDate = new Date(TODAY);
-        endDate.setDate(endDate.getDate() + 6)
+        endDate.setDate(endDate.getDate() + 6);
 
         getMovieScreeningsInCinema(movieId, id, TODAY.toISOString(), endDate.toISOString())
             .then((data) => setScreenings(data))
             .catch((e) => console.log(e));
-    }, [])
+    }, []);
 
     console.log(screenings);
-    
-
-
 
     return (
         <>
             <h2>Screening</h2>
             <div className="flex gap-3">
-                {week.map((date) => (
-                    <ScreeningDate key={date} date={date} />
-                ))}
+                {week.map((date) => {
+                    const s = screenings?.filter((screen) => {
+                        const copy = new Date(screen.startTime);
+                        return new Intl.DateTimeFormat("da-DK", OPTIONS).format(copy) == date;
+                    }) as IScreening[];
+                    return <ScreeningDate key={date} date={date} screenings={s} />;
+                })}
             </div>
         </>
     );
