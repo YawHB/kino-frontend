a4542a63763468e2d5ffb1f3a783bafb3201c0fc
Forkeh <forkeh@gmail.com>
2024-03-19 11:48:30 +0100

    shows total price sum

--- src/components/core/SeatPricing.tsx
+++ src/components/core/SeatPricing.tsx
@@ -8,26 +8,74 @@ interface Props {
   is3D?: boolean;
 }
 
+function calculatSeatsPrice(seats: Iseat[]) {
+  return seats.reduce((sum, currSeat) => sum + currSeat.seatPricing.price, 0);
+}
+
 export default function SeatPricing({ seats, runtime, is3D }: Props) {
-  const DUMMY_MOVIE_RUNTIME = 160;
-  const DUMMY_MOVIE_IS3D = false;
+  const COWBOY_SEATS: Iseat[] = [];
+  const STANDARD_SEATS: Iseat[] = [];
+  const DELUXE_SEATS: Iseat[] = [];
+  console.log(seats);
+
+  const DUMMY_MOVIE_RUNTIME = 180;
+  const DUMMY_MOVIE_IS3D = true;
 
   const GROUP_PRICING_ADJUSTMENT =
     seats.length <= 5 ? 1.05 : seats.length > 10 ? 0.97 : 1;
 
-  const TOTAL_SEAT_PRICE =
-    seats.reduce((sum, currSeat) => sum + currSeat.seatPricing.price, 0) *
-    GROUP_PRICING_ADJUSTMENT;
+  const TOTAL_SEAT_PRICE = calculatSeatsPrice(seats) * GROUP_PRICING_ADJUSTMENT;
 
   const RUNTIME_FEE = DUMMY_MOVIE_RUNTIME >= 170 ? 10 : 0;
-  console.log("runtime fee: " + RUNTIME_FEE);
-  
+
   const FEE_3D = DUMMY_MOVIE_IS3D ? 15 : 0;
-  console.log("3d fee: " + FEE_3D);
 
-    
-  console.log(seats);
-  console.log(GROUP_PRICING_ADJUSTMENT);
+  for (const seat of seats) {
+    if (seat.seatPricing.name === "cowboy") COWBOY_SEATS.push(seat);
+    if (seat.seatPricing.name === "standard") STANDARD_SEATS.push(seat);
+    if (seat.seatPricing.name === "deluxe") DELUXE_SEATS.push(seat);
+  }
 
-  return <div>Total price - {TOTAL_SEAT_PRICE}</div>;
+  console.log("Cowboys length");
+  console.log(COWBOY_SEATS.length);
+
+  return (
+    <>
+      <div className="font-bold">Seats</div>
+      {COWBOY_SEATS.length > 0 && (
+        <div>
+          {COWBOY_SEATS.length}x Cowboy - {calculatSeatsPrice(COWBOY_SEATS)}kr.
+        </div>
+      )}
+      {STANDARD_SEATS.length > 0 && (
+        <div>
+          {STANDARD_SEATS.length}x Standard -{" "}
+          {calculatSeatsPrice(STANDARD_SEATS)}
+          kr.
+        </div>
+      )}
+      {DELUXE_SEATS.length > 0 && (
+        <div>
+          {DELUXE_SEATS.length}x Deluxe - {calculatSeatsPrice(DELUXE_SEATS)}kr.
+        </div>
+      )}
+      <div>Total seats price - {TOTAL_SEAT_PRICE}kr.</div>
+      {DUMMY_MOVIE_IS3D && (
+        <div>
+          3D Fee ({seats.length} x {FEE_3D}kr.) - {FEE_3D * seats.length}kr.
+        </div>
+      )}
+
+      {RUNTIME_FEE > 0 && (
+        <div>
+          Runtime fee ({seats.length} x {RUNTIME_FEE}kr.) -{" "}
+          {RUNTIME_FEE * seats.length}kr.
+        </div>
+      )}
+
+      <div>
+        I Alt - {TOTAL_SEAT_PRICE + (FEE_3D + RUNTIME_FEE) * seats.length}kr
+      </div>
+    </>
+  );
 }
