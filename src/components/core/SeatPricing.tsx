a6858d07c8b45708add9dfa2bce65d2efb2cd465
Forkeh <forkeh@gmail.com>
2024-03-19 10:26:03 +0100

    shows seat group price adjustments

--- src/components/core/SeatPricing.tsx
+++ src/components/core/SeatPricing.tsx
@@ -1,18 +1,22 @@
-import { Iseat } from '@/models/seat'
-import React from 'react'
-import { SiUbereats } from 'react-icons/si';
+import { Iseat } from "@/models/seat";
+import React from "react";
+import { SiUbereats } from "react-icons/si";
 
 interface Props {
-    seats: Iseat[]
+  seats: Iseat[];
 }
 
+export default function SeatPricing({ seats }: Props) {
+  const GROUP_PRICING_ADJUSTMENT =
+    seats.length <= 5 ? 1.05 : seats.length > 10 ? 0.97 : 1;
 
-export default function SeatPricing({seats}: Props) {
-    const TOTAL_SEAT_PRICE = seats.reduce((sum, currSeat) =>  sum + currSeat.seatPricing.price, 0 )
-    
-    console.log(seats);
-    
-  return (
-    <div>Total price - {TOTAL_SEAT_PRICE}</div>
-  )
+  const TOTAL_SEAT_PRICE =
+    seats.reduce((sum, currSeat) => sum + currSeat.seatPricing.price, 0) *
+    GROUP_PRICING_ADJUSTMENT;
+
+  console.log(seats);
+  console.log(GROUP_PRICING_ADJUSTMENT);
+  
+
+  return <div>Total price - {TOTAL_SEAT_PRICE}</div>;
 }
