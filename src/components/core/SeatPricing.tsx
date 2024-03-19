a91e86d0cfad948eea33a35b5f2749b99bd31104
Forkeh <forkeh@gmail.com>
2024-03-19 10:45:15 +0100

    shows runtime and 3d fee

--- src/components/core/SeatPricing.tsx
+++ src/components/core/SeatPricing.tsx
@@ -4,9 +4,14 @@ import { SiUbereats } from "react-icons/si";
 
 interface Props {
   seats: Iseat[];
+  runtime?: number;
+  is3D?: boolean;
 }
 
-export default function SeatPricing({ seats }: Props) {
+export default function SeatPricing({ seats, runtime, is3D }: Props) {
+  const DUMMY_MOVIE_RUNTIME = 160;
+  const DUMMY_MOVIE_IS3D = false;
+
   const GROUP_PRICING_ADJUSTMENT =
     seats.length <= 5 ? 1.05 : seats.length > 10 ? 0.97 : 1;
 
@@ -14,9 +19,15 @@ export default function SeatPricing({ seats }: Props) {
     seats.reduce((sum, currSeat) => sum + currSeat.seatPricing.price, 0) *
     GROUP_PRICING_ADJUSTMENT;
 
+  const RUNTIME_FEE = DUMMY_MOVIE_RUNTIME >= 170 ? 10 : 0;
+  console.log("runtime fee: " + RUNTIME_FEE);
+  
+  const FEE_3D = DUMMY_MOVIE_IS3D ? 15 : 0;
+  console.log("3d fee: " + FEE_3D);
+
+    
   console.log(seats);
   console.log(GROUP_PRICING_ADJUSTMENT);
-  
 
   return <div>Total price - {TOTAL_SEAT_PRICE}</div>;
 }
