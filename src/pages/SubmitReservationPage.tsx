356cd36610e6bf8dca85318c7c9947f030589996
Forkeh <forkeh@gmail.com>
2024-03-21 10:44:41 +0100

    fetching final reservation price from backend

--- src/pages/SubmitReservationPage.tsx
+++ src/pages/SubmitReservationPage.tsx
@@ -1,27 +1,58 @@
-import {createReservation, TReservationRequest} from "@/services/apiFacade.ts";
-import {toast} from "@/components/ui/use-toast.ts";
-import {useLocation, useParams} from "react-router-dom";
-import {Iseat} from "@/models/seat.ts";
+import { createReservation, getCalculatedReservationPrice, TReservationPriceRequest, TReservationRequest } from "@/services/apiFacade.ts";
+import { toast } from "@/components/ui/use-toast.ts";
+import { useLocation, useParams } from "react-router-dom";
+import { Iseat } from "@/models/seat.ts";
 import SeatPricing from "@/components/core/SeatPricing.tsx";
 import Button from "@/components/core/Button.tsx";
+import { useEffect, useState } from "react";
 
+export type TCalculatedPrice = {
+    seats: number;
+    fees: number;
+    discounts: number;
+    totalPrice: number;
+};
 
 export default function SubmitReservationPage() {
-    const {screeningId} = useParams();
-    const selectedSeats = useLocation().state as Iseat[]
+    const [calculatedPrice, setCalculatedPrice] = useState<TCalculatedPrice | null>(null);
+
+    const { screeningId } = useParams();
+    const selectedSeats = useLocation().state as Iseat[];
+    const seatIds = selectedSeats.map((seat) => seat.id);
+
+    const request: TReservationPriceRequest = {
+        seatIds,
+        screeningId: Number(screeningId),
+    };
+
+    useEffect(() => {
+        getCalculatedReservationPrice(request)
+            .then((data) => {
+                console.log(data);
+                
+                setCalculatedPrice(data)})
+            .catch((e) => {
+                console.log(e);
+                
+                toast({
+                    title: "Something went wrong!",
+                    description: `Could not calculate the prices in our system. Please reload the webpage or try again at a later time.`,
+                    variant: "destructive",
+                });
+            });
+    }, []);
 
     function handleSubmitReservation() {
         const request: TReservationRequest = {
             screeningId: Number(screeningId as string),
-            seatIds: selectedSeats.map((s) => s.id)
-        }
+            seatIds: selectedSeats.map((s) => s.id),
+        };
 
         createReservation(request)
             .then(() => {
                 toast({
                     title: "Reservation Created!",
                     description: "Success!",
-
                 });
             })
             .catch(() => {
@@ -30,7 +61,7 @@ export default function SubmitReservationPage() {
                     description: "Could not create reservation. Try again later",
                     variant: "destructive",
                 });
-            })
+            });
     }
 
     return (
@@ -40,5 +71,5 @@ export default function SubmitReservationPage() {
                 <Button onClick={handleSubmitReservation}>Buy</Button>
             </div>
         </>
-    )
+    );
 }
