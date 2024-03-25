bf457347ab20f43f8d2c9961a70acbacfc3ce49a
Forkeh <forkeh@gmail.com>
2024-03-25 01:46:46 +0100

    minor fixes

--- src/components/core/Ticket.tsx
+++ src/components/core/Ticket.tsx
@@ -6,11 +6,11 @@ interface Props {
 
 export default function Ticket({ seat }: Props) {
     return (
-        <div className="flex h-14 w-[120px] scale-75 sm:scale-100 items-center justify-center bg-[url('ticket-shape.svg')] bg-no-repeat text-white gap-5">
-            <div className="">
+        <div className="flex h-14 w-[120px] scale-75 items-center justify-center gap-5 bg-[url('ticket-shape.svg')] bg-no-repeat text-white sm:scale-100">
+            <div className="opacity-80 text-center">
                 Row <span className="block font-bold">{seat.rowNumber}</span>
             </div>
-            <div>
+            <div className="opacity-80 text-center">
                 Seat <span className="block font-bold">{seat.seatNumber}</span>
             </div>
         </div>
--- src/components/layouts/TicketsDisplay.tsx
+++ src/components/layouts/TicketsDisplay.tsx
@@ -9,7 +9,7 @@ interface Props {
 export default function TicketsDisplay({ selectedSeats }: Props) {
     return (
         <div className="relative">
-            <div className="pointer-events-none absolute opacity-80 z-10 h-full w-full bg-[linear-gradient(90deg,rgba(100,116,139,1)0%,rgba(0,0,0,0)5%,rgba(0,0,0,0)95%,rgba(100,116,139,1)100%)]"></div>
+            <div className="pointer-events-none absolute z-10 h-full w-full bg-[linear-gradient(90deg,rgba(100,116,139,1)0%,rgba(0,0,0,0)5%,rgba(0,0,0,0)95%,rgba(100,116,139,1)100%)]"></div>
             <Carousel>
                 <CarouselPrevious variant={"ghost"} />
                 <CarouselContent>
